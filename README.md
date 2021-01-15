12b9140b63eabf03bfafe646b0a3b0a48e6a6b67
shinjeongmin <sin37minduke@naver.com>
2021-01-15 17:14:08 +0900

    modify server.js

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # READEME.md
 
 https://www.youtube.com/watch?v=DvlyzDZDEq4
+
+peerjs 설치
+npm i -g peer
+
+peerjs 실행
+peer --port 3001(포트)
+
+nodemon 설치
+npm i --save-dev nodemon
+
+nodemon 실행
+npm run devStart
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require('express')
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
 const app = express()
-const server = require('http').Server(app)
+const server = require('https').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
+
+const options = {
+    // 공개키와 cert가 있는 서버에서의 경로
+    key : fs.readFileSync('/etc/letsencrypt/live/mylifeisgood.cf/privkey.pem', 'utf8'),
+    cert : fs.readFileSync('/etc/letsencrypt/live/mylifeisgood.cf/fullchain.pem', 'utf8')
+};
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -27,4 +36,6 @@ io.on('connection', socket => {
     })
 })
 
-server.listen(3000)
+//server.listen(3000)
+
+https.createServer(options, app).listen(3000);
